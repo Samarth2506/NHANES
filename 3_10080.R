4524ce3f872b637b89daf96191f0e918b6ccdb41
Luchao Qi <lc.qi@outloook.com>
2019-08-17 16:46:07 -0400

    lc

--- 3_10080.R
+++ 3_10080.R
@@ -1,4 +1,4 @@
-##########################################################################################################################################
+#########################################################################
 # re-encode time-series 7*1440 matrix to 1*(1440*7) matrix
 
 rm(list = ls())
@@ -49,6 +49,11 @@ if(T){
 if(F){
   save(analyticData,file = "10080.rda")
 }
+
+
+#########################################################################
+# 1*10080 DL 
+
 y = analyticData$permth %>% as.matrix()
 x = analyticData %>% select(-SEQN) %>% as.matrix()
 
@@ -110,3 +115,141 @@ table(ytest = y[!trainIdx, ],model %>% predict_classes(xtest))
 #     0  26
 #     1  39
 #     2 409
+
+
+#########################################################################
+# 10080 based PCA DL
+
+rm(list = ls())
+load(file = "10080.rda")
+analyticData[1:5,1:5]
+analyticData[1:5,10070:10082]
+
+
+pca = prcomp(analyticData %>% select(-SEQN,-permth),
+             center = T,
+             scale = T)
+
+
+y = analyticData$permth %>% as.matrix()
+x = pca$x  %>% as.matrix()
+
+set.seed(1)
+trainIdx = sample(nrow(y),nrow(y)*0.75)
+
+ytrain = y[trainIdx, ]
+xtrain = x[trainIdx, ]
+
+
+xtest = x[-trainIdx, ]   
+ytest = y[-trainIdx, ]
+
+table(ytrain)
+table(ytest)
+ytrain <- to_categorical(ytrain,3)
+ytest <- to_categorical(ytest, 3)
+
+
+model <- keras_model_sequential() %>% 
+  layer_dense(units = 2^8, activation = 'relu', input_shape = dim(xtrain)[2]) %>% 
+  layer_dropout(rate = 0.4) %>% 
+  layer_dense(units = 2^4, activation = 'relu') %>%
+  layer_dropout(rate = 0.3) %>%
+  layer_dense(units = 3, activation = 'softmax')
+
+model %>% compile(
+  loss = 'categorical_crossentropy',
+  optimizer = optimizer_rmsprop(),
+  metrics = c('accuracy')
+)
+
+history <- model %>% fit(
+  xtrain, ytrain, 
+  epochs = 30, 
+  batch_size = 128,
+  validation_split = 0.2
+)
+
+plot(history)
+
+model %>% evaluate(xtest, ytest)
+
+# $loss
+# [1] 1.2907
+# 
+# $acc
+# [1] 0.8605898
+
+table(ytest = y[-trainIdx, ],model %>% predict_classes(xtest))
+
+
+# ytest   0   1   2
+#     0   1   0  21
+#     1   3   2  26
+#     2   0   2 318
+
+###################
+# spectrum DL
+
+rm(list = ls())
+load(file = "10080.rda")
+x = apply(analyticData %>% select(-SEQN,-permth),1, FUN = function(i){
+  data = i %>% spectrum()
+  return(data$spec)
+}) %>% t()
+
+y = analyticData$permth %>% as.matrix()
+
+set.seed(1)
+trainIdx = sample(nrow(y),nrow(y)*0.75)
+
+ytrain = y[trainIdx, ]
+xtrain = x[trainIdx, ]
+
+
+xtest = x[-trainIdx, ]   
+ytest = y[-trainIdx, ]
+
+table(ytrain)
+table(ytest)
+ytrain <- to_categorical(ytrain,3)
+ytest <- to_categorical(ytest, 3)
+
+
+model <- keras_model_sequential() %>% 
+  layer_dense(units = 2^10, activation = 'relu', input_shape = dim(xtrain)[2]) %>% 
+  layer_dropout(rate = 0.4) %>% 
+  layer_dense(units = 2^5, activation = 'relu') %>%
+  layer_dropout(rate = 0.3) %>%
+  layer_dense(units = 3, activation = 'softmax')
+
+model %>% compile(
+  loss = 'categorical_crossentropy',
+  optimizer = optimizer_rmsprop(),
+  metrics = c('accuracy')
+)
+
+history <- model %>% fit(
+  xtrain, ytrain, 
+  epochs = 30, 
+  batch_size = 128,
+  validation_split = 0.2
+)
+
+plot(history)
+
+model %>% evaluate(xtest, ytest)
+
+# $loss
+# [1] 2.290239
+# 
+# $acc
+# [1] 0.8579088
+
+table(ytest = y[-trainIdx, ],model %>% predict_classes(xtest))
+
+
+# ytest   2
+#     0  22
+#     1  31
+#     2 320
