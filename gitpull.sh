701d3c81b2a58582897eea003d0701e2a0223780
Luchao Qi <lc.qi@outlook.com>
2019-10-01 13:15:50 -0400

    Luchao's default auto-commitment using gitpull.sh

--- gitpull.sh
+++ gitpull.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+#find ./* -size +100M | cat >> .gitignore
 git pull
 git add .
-git commit -m ${1:-lc}
+git commit -m ${1:-"Luchao's default auto-commitment using gitpull.sh"}
 git push
+
+/bin/bash
